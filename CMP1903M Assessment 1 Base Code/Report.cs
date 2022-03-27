7a9f68cf6a70dc050ae8fe1703e28310bfdbf987
Connor <101995232+ConnorWGraves@users.noreply.github.com>
2022-03-28 00:28:40 +0100

    Update Report.cs

--- CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/Report.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +12,16 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Handles the reporting of the analysis
         public void decision(int Measurements)
         {
-            Console.Write.Line(measurements);
+            Console.WriteLine(Measurements);
+        }
+
+        internal void decision(string[] measurements)
+        {
+            throw new NotImplementedException();
         }
         //Maybe have different methods for different formats of output?
         //eg.   public void outputConsole(List<int>)
-        
+
 
     }
 }
